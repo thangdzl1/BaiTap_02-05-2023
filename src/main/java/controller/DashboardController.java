57326d94403b1fe4a0e17b4583e695271079e94a
BQcThang <quocthang6a1@gmail.com>
2023-05-04 11:01:09 +0700

    initial commit

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "DashboardController", urlPatterns = {"/dashboard"})
+@WebServlet(name = "dashboardController", urlPatterns = {"/dashboard"})
 public class DashboardController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
+        req.getRequestDispatcher("user-table.jsp").forward(req, resp);
     }
 
     @Override
